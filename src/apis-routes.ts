f0c1557038b48cea602d6e546e7d1d7ed2960c5b
Luis0714 <94072533+Luis0714@users.noreply.github.com>
2023-06-27 09:02:53 -0500

    Update apis-routes.ts

--- src/apis-routes.ts
+++ src/apis-routes.ts
@@ -2,5 +2,4 @@ export const API_ROUTES = {
   UserUrl: 'https://localhost:7029/api/v1.0/user',
   AccountUrl: 'https://localhost:7029/api/v1.0/account',
   FileUrl: 'https://localhost:7029/api/v1.0/file',
-  firebase: { apiKey: 'AIzaSyCN599W6_flBEpe0xrcuBlGPRW3OKzDr7o', authDomain: 'platzi-store-forms.firebaseapp.com', databaseURL: 'https://platzi-store-forms.firebaseio.com', projectId: 'platzi-store-forms', storageBucket: 'platzi-store-forms.appspot.com', messagingSenderId: '783669493891', appId: '1:783669493891:web:f973c1ba9bb213e2c75bb6' },
 };
